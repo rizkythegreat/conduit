34b74175a675307e5df250cd7a660356930ceae3
Rizky Rahman Salam <clashikik@gmail.com>
2025-05-29 20:53:28 +0700

    feat: add article form

--- src/components/templates/HomepageScreen.jsx
+++ src/components/templates/HomepageScreen.jsx
@@ -1,10 +1,14 @@
+import AuthContext from '@/context/AuthContext';
 import AuthLayout from '@/layouts/AuthLayout';
-import React from 'react';
+import React, { useContext } from 'react';
 
 function HomepageScreen() {
+  const { user } = useContext(AuthContext);
   return (
     <AuthLayout>
-      <h1 className="text-4xl font-sohne">Welcome...</h1>
+      <h1 className="text-lg font-sohne">
+        Hi {user?.displayName}, <br /> how are you?
+      </h1>
       <div className="mt-8">
         {/* Konten utama profile */}
         <p>Your homepage content goes here...</p>
--- src/components/templates/NewArticleScreen.jsx
+++ src/components/templates/NewArticleScreen.jsx
@@ -1,5 +1,6 @@
 import AuthLayout from '@/layouts/AuthLayout';
 import React from 'react';
+import Box from '../atom/Box';
 
 function NewArticleScreen() {
   return (
@@ -8,7 +9,39 @@ function NewArticleScreen() {
         <h1 className="text-4xl font-sohne">New Article</h1>
         <div className="mt-8">
           {/* Konten utama profile */}
-          <p>Your new article content goes here...</p>
+          <Box>
+            <input
+              type="text"
+              placeholder="Title"
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4"
+            />
+            <input
+              type="text"
+              placeholder="Description"
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4"
+            />
+            <textarea
+              type="text"
+              rows="4"
+              placeholder="Body"
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4"
+            />
+            <input
+              type="text"
+              placeholder="Tags"
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4"
+            />
+            <input
+              type="file"
+              placeholder="Image"
+              accept="image/*"
+              onChange={(e) => console.log(e.target.files[0])}
+              className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4 file:mr-4 file:bg-blue-500 file:text-white file:px-3 file:py-1 file:rounded-full file:hover:bg-blue-600 file:transition-btn file:cursor-pointer"
+            />
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-btn">
+              Publish Article
+            </button>
+          </Box>
         </div>
       </AuthLayout>
     </>
